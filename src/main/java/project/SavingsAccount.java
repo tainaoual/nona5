b5f86d69d02edb9827c2453f6cd129c97654b52d
tainaoual <Tai Naoual>
2020-09-21 22:17:14 -0400

    assesment change

---
+++ src/main/java/project/SavingsAccount.java
@@ -0,0 +1,54 @@
+package project;
+public class SavingsAccount {
+    private final double balance;
+    private final int accountNumber;
+    private final double interestRate;
+    private double deposit;
+    private double withdraw;
+    private double interest;
+    public SavingsAccount(double balance, int accountNumber, double interestRate) {
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+        this.interestRate = interestRate;
+    }
+    public double getBalance() {
+        if (deposit > 0) {
+            return (balance + deposit + interest);
+        } else if (withdraw > 0) {
+            return (balance - withdraw);
+        } else if (interest > 0) {
+            return (balance + deposit + interest);
+        }
+        return balance;
+    }
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+    public double getInterestRate() {
+        return interestRate;
+    }
+    public void deposit(double v) {
+        deposit = v;
+    }
+    public void withdraw(double v) {
+        withdraw = v;
+        if (balance - withdraw <= 0) {
+            withdraw = 0;
+        }
+    }
+    public void accrueInterest() {
+        interest = balance * interestRate;
+        if (deposit > 0) {
+            interest = (balance + deposit) * interestRate + balance;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
